820fca6abad83596ce0b5e219247a8e76d259937
TheDemarti <simone.demartinis@gmail.com>
2020-06-07 23:21:51 +0200

    Initial commit

---
+++ app/src/main/java/it/giudevo/worldbank/MainActivity.java
@@ -0,0 +1,14 @@
+android package it.giudevo.worldbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
