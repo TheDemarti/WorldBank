7a9ee9d6fd91aefa3ee1fdb58e561356b5286ab3
giogiu98 <gio.giunta@gmail.com>
2020-06-09 16:06:03 +0200

    parte1

--- app/src/main/java/it/giudevo/worldbank/MainActivity.java
+++ app/src/main/java/it/giudevo/worldbank/MainActivity.java
@@ -2,7 +2,12 @@ package it.giudevo.worldbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import it.giudevo.worldbank.searchapi.SearchByCountry;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Holder();
     }
+
+
+
+
+ class Holder implements View.OnClickListener{
+   Button btnCountry, btnArg, btnOff;
+
+        Holder(){
+        btnArg = findViewById(R.id.btnArg);
+        btnCountry = findViewById(R.id.btnCountry);
+        btnOff= findViewById(R.id.btnOff);
+
+        }
+
+
+     @Override
+     public void onClick(View v) {
+         if(v.getId() == R.id.btnArg){
+             Intent intent = new Intent(MainActivity.this, SearchByCountry.class);
+
+             SearchByCountry.this.startActivity(intent);
+         }
+     }
+ }
 }
+
---
+++ app/src/main/java/it/giudevo/worldbank/searchapi/SearchByCountry.java
@@ -0,0 +1,14 @@
+package it.giudevo.worldbank.searchapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchByCountry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_by_country);
+    }
+}
