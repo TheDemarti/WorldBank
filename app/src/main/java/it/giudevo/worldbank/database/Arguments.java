fcebb5a806979473dda5262d2d2b411487902698
derrik98 <danielederrico98@gmail.com>
2020-06-11 11:22:51 +0200

    daniele

--- app/src/main/java/it/giudevo/worldbank/database/Arguments.java
+++ app/src/main/java/it/giudevo/worldbank/database/Arguments.java
@@ -4,27 +4,61 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 import androidx.room.ColumnInfo;
+import androidx.room.Embedded;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-@Entity
-public class Arguments implements Parcelable {
-    @PrimaryKey(autoGenerate = true)
-//    @ColumnInfo(name="page")
-//    public int page;
-//    @ColumnInfo(name="pages")
-//    public int pages;
-//    @ColumnInfo(name="per_page")
-//    public String per_page;
-//    @ColumnInfo(name="total")
-//    public int total;
-    @ColumnInfo(name="id")
-    public int id;
-    @ColumnInfo(name="value")
-    public String value;
-    @ColumnInfo(name="sourceNote")
-    public String sourceNote;
+import java.util.ArrayList;
+import java.util.List;
 
+@Entity(tableName = "Arg")
+public class Arguments implements Parcelable{
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "page")
+    public int page;
+    @ColumnInfo(name = "pages")
+    public int pages;
+    @ColumnInfo(name = "per_page")
+    public String per_page;
+    @ColumnInfo(name = "total")
+    public int total;
+    @Embedded
+    private ArrayList<Template> templateList;
+
+    public Arguments(){
+
+    }
+
+    public void setTemplateList(ArrayList<Template> templateList) {
+        this.templateList = templateList;
+    }
+
+    public ArrayList<Template> getTemplateList() {
+        return templateList;
+    }
+
+    protected Arguments(Parcel in) {
+        this.page = (Integer) in.readInt();
+        this.pages = in.readInt();
+        this.per_page = in.readString();
+        this.total = in.readInt();
+        this.templateList = in.createTypedArrayList(Template.CREATOR);
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(page);
+        dest.writeInt(pages);
+        dest.writeString(per_page);
+        dest.writeInt(total);
+        dest.writeTypedList(templateList);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
 
     public static final Creator<Arguments> CREATOR = new Creator<Arguments>() {
         @Override
@@ -37,6 +71,63 @@ public class Arguments implements Parcelable {
             return new Arguments[size];
         }
     };
+}
+
+@Entity(tableName = "Templates")
+class Template implements Parcelable{
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int id;
+    @ColumnInfo(name = "value")
+    private String value;
+    @ColumnInfo(name = "sourceNote")
+    private String sourceNote;
+
+    public Template(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getSourceNote() {
+        return sourceNote;
+    }
+
+    public void setSourceNote(String sourceNote) {
+        this.sourceNote = sourceNote;
+    }
+
+    protected Template(Parcel in) {
+        this.id = (Integer)in.readInt();
+        this.value = in.readString();
+        this.sourceNote = in.readString();
+    }
+
+    public static final Creator<Template> CREATOR = new Creator<Template>() {
+        @Override
+        public Template createFromParcel(Parcel in) {
+            return new Template(in);
+        }
+
+        @Override
+        public Template[] newArray(int size) {
+            return new Template[size];
+        }
+    };
 
     @Override
     public int describeContents() {
@@ -45,22 +136,111 @@ public class Arguments implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
-//        dest.writeInt(page);
-//        dest.writeInt(pages);
-//        dest.writeString(per_page);
-//        dest.writeInt(total);
         dest.writeInt(id);
         dest.writeString(value);
         dest.writeString(sourceNote);
     }
+}
 
-    protected Arguments(Parcel in) {
+//@Entity
+//public class Arguments implements Parcelable {
+//    @PrimaryKey(autoGenerate = true)
+//    @ColumnInfo(name="page")
+//    public int page;
+//    @ColumnInfo(name="pages")
+//    public int pages;
+//    @ColumnInfo(name="per_page")
+//    public String per_page;
+//    @ColumnInfo(name="total")
+//    public int total;
+//    @Embedded
+//    private Arg mArg;
+//
+//    protected Arguments(Parcel in) {
 //        page = in.readInt();
 //        pages = in.readInt();
 //        per_page = in.readString();
 //        total = in.readInt();
-        id = in.readInt();
-        value = in.readString();
-        sourceNote = in.readString();
-    }
-}
+//    }
+//
+//    public static final Creator<Arguments> CREATOR = new Creator<Arguments>() {
+//        @Override
+//        public Arguments createFromParcel(Parcel in) {
+//            return new Arguments(in);
+//        }
+//
+//        @Override
+//        public Arguments[] newArray(int size) {
+//            return new Arguments[size];
+//        }
+//    };
+//
+//    @Override
+//    public int describeContents() {
+//        return 0;
+//    }
+//
+//    @Override
+//    public void writeToParcel(Parcel dest, int flags) {
+//        dest.writeInt(page);
+//        dest.writeInt(pages);
+//        dest.writeString(per_page);
+//        dest.writeInt(total);
+//    }
+//
+//    public static class Arg{
+//        @ColumnInfo(name="id")
+//        public int id;
+//        @ColumnInfo(name="value")
+//        public String value;
+//        @ColumnInfo(name="sourceNote")
+//        public String sourceNote;
+//    }
+//
+
+//    @ColumnInfo(name="id")
+//    public int id;
+//    @ColumnInfo(name="value")
+//    public String value;
+//    @ColumnInfo(name="sourceNote")
+//    public String sourceNote;
+
+
+
+//    public static final Creator<Arguments> CREATOR = new Creator<Arguments>() {
+//        @Override
+//        public Arguments createFromParcel(Parcel in) {
+//            return new Arguments(in);
+//        }
+//
+//        @Override
+//        public Arguments[] newArray(int size) {
+//            return new Arguments[size];
+//        }
+//    };
+//
+//    @Override
+//    public int describeContents() {
+//        return 0;
+//    }
+//
+//    @Override
+//    public void writeToParcel(Parcel dest, int flags) {
+//        dest.writeInt(page);
+//        dest.writeInt(pages);
+//        dest.writeString(per_page);
+//        dest.writeInt(total);
+//        dest.writeInt(id);
+//        dest.writeString(value);
+//        dest.writeString(sourceNote);
+//    }
+//
+//    protected Arguments(Parcel in) {
+//        page = in.readInt();
+//        pages = in.readInt();
+//        per_page = in.readString();
+//        total = in.readInt();
+//        id = in.readInt();
+//        value = in.readString();
+//        sourceNote = in.readString();
+//    }
--- app/src/main/java/it/giudevo/worldbank/database/ArgumentsDAO.java
+++ app/src/main/java/it/giudevo/worldbank/database/ArgumentsDAO.java
@@ -8,26 +8,55 @@ import androidx.room.Query;
 
 import java.util.List;
 
+import javax.xml.transform.Templates;
+
+//@Dao
+//public interface ArgumentsDAO {
+//    @Query("SELECT * FROM arguments")
+//    List<Arguments> getAll();
+//
+//    @Query("SELECT * FROM Arguments WHERE id IN (:ids)")
+//    List<Arguments> loadAllByIds(int[] ids);
+//
+//    @Query("SELECT * FROM arguments WHERE value LIKE :value ORDER BY value")
+//    List<Arguments> findByCountryName(String value);
+//
+//    @Query("SELECT count(*) FROM arguments")
+//    int size();
+//
+//    @Insert(onConflict = OnConflictStrategy.REPLACE)
+//    void insertAll(Arguments... arguments);
+//
+//    @Insert(onConflict = OnConflictStrategy.REPLACE)
+//    void insertAll(List<Arguments>arguments);
+//
+//    @Delete
+//    void delete(Arguments arguments);
+//}
+
 @Dao
 public interface ArgumentsDAO {
-    @Query("SELECT * FROM arguments")
+    @Insert
+    void insert (Template template);
+
+    @Query("SELECT * FROM Arg")
     List<Arguments> getAll();
 
-    @Query("SELECT * FROM Arguments WHERE id IN (:ids)")
-    List<Arguments> loadAllByIds(int[] ids);
+//    @Query("SELECT * FROM Arg.Templates " + " WHERE id IN (:ids)")
+//    List<Arguments> loadAllByIds(int[] ids);
 
-    @Query("SELECT * FROM arguments WHERE value LIKE :value ORDER BY value")
-    List<Arguments> findByCountryName(String value);
+    @Query("SELECT * FROM " + " Arg.Templates WHERE value LIKE :value ORDER BY value")
+    List<Template> findByCountryName(String value);
 
-    @Query("SELECT count(*) FROM arguments")
+    @Query("SELECT count(*) FROM Arg")
     int size();
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    void insertAll(Arguments... arguments);
+    void insertAll(Template... templates);
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    void insertAll(List<Arguments>arguments);
+    void insertAll(List<Template>templates);
 
     @Delete
-    void delete(Arguments arguments);
+    void delete(Template templates);
 }
