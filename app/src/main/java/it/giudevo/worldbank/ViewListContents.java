127d36f4fde538db1fb4bb0efc256b992086036b
derrik98 <danielederrico98@gmail.com>
2020-07-06 16:35:29 +0200

    6/07/2020 daniele

--- app/src/main/java/it/giudevo/worldbank/ViewListContents.java
+++ app/src/main/java/it/giudevo/worldbank/ViewListContents.java
@@ -39,11 +39,9 @@ public class ViewListContents extends AppCompatActivity {
 
     DataBaseHelper myDB;
     RecyclerView rvFav;
-    public Cursor res;
     public Cursor data;
     public SparseBooleanArray selectedList = new SparseBooleanArray();
     public ArrayList<String> theList;
-    //public ArrayAdapter<String> listAdapter;
     public FinAdapter finAdapter;
 
     @Override
@@ -54,98 +52,23 @@ public class ViewListContents extends AppCompatActivity {
         rvFav = findViewById(R.id.rvFav);
         myDB = new DataBaseHelper(this);
 
-        //populate an ArrayList<String> from the database and then view it
+
         theList = new ArrayList<>();
-        //public final Cursor
+
         data = myDB.getListContents();
         if (data.getCount() == 0) {
             Toast.makeText(this, R.string.no_content, Toast.LENGTH_LONG).show();
         } else {
             while (data.moveToNext()) {
                 theList.add(data.getString(1));
-                Log.w("CA", data.getString(1));
-
 
                 rvFav.setLayoutManager(new LinearLayoutManager((this)));
-                finAdapter = new FinAdapter(data);
+                finAdapter = new FinAdapter(theList);
                 rvFav.setAdapter(finAdapter);
 
-
-//                listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, theList);
-//                Log.w("CA", "RESTORE ADAPTER");
-//                rvFav.setAdapter(listAdapter);
-//                Log.w("CA", "RESTORE ADAPTER");
-//                rvFav.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-//                    @Override
-//                    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-//
-//                        boolean isSelected = selectedList.get(position,false);
-//                        if(isSelected) {
-//                            parent.setSelected(false);
-//                            selectedList.delete(position);
-//                            parent.getChildAt(position).setBackgroundColor(Color.WHITE);
-//                        } else {
-//                            parent.setSelected(true);
-//                            selectedList.put(position, true);
-//                            parent.getChildAt(position).setBackgroundColor(Color.GRAY);
-//                        }
-//                        Log.w("CA", String.valueOf(selectedList.size()));
-//                        listAdapter.notifyDataSetChanged();
-//                        return true;
-//                    }
-//                });
-//                rvFav.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//                    @Override
-//                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                        Intent intent = new Intent(ViewListContents.this, showDetails.class);
-//                        //parent.getAdapter().getItem(position);
-//                        intent.putExtra("details", String.valueOf(parent.getAdapter().getItem(position)));
-//                        //Log.w("CA", data.getString(0));
-//                        ViewListContents.this.startActivity(intent);
-//                        Log.w("CA", (String) parent.getAdapter().getItem(position));
-//                    }
-//                });
-
             }
-            Log.w("CA", "la lista e" + theList);
         }
     }
-
-//    @Override
-//    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-//
-//
-//        boolean isSelected = selectedList.get(position,false);
-//        if(isSelected) {
-//            parent.setSelected(true);
-//            selectedList.delete(position);
-//            parent.getChildAt(position).setBackgroundColor(Color.WHITE);
-//        }
-//        else {
-//            parent.setSelected(true);
-//            selectedList.put(position, true);
-//            parent.getChildAt(position).setBackgroundColor(Color.GRAY);
-//        }
-//        Log.w("CA", String.valueOf(selectedList.size()));
-////////////////////////////////////////////////////////////////////////////////////
-////        boolean isSel = theList.get(position).isEmpty();
-////
-////        if (isSel) {
-////            parent.setSelected(false);
-////            selectedList.delete(position);/////////////////////////////////////////
-////            parent.getChildAt(position).setBackgroundColor(Color.RED);
-////        } else {
-////            parent.setSelected(true);
-////            selectedList.put(position, true);
-////            parent.getChildAt(position).setBackgroundColor(Color.GREEN);
-////            //String idd = theList.get(position);
-////            //myDB.deletedata(idd);
-////            //theList.remove(position);
-////        }
-//        listAdapter.notifyDataSetChanged();
-//        return true;
-//
-//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -154,105 +77,20 @@ public class ViewListContents extends AppCompatActivity {
     }
 
 
-//    ActionMode mActionMode;
-//
-//    private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
-//        @Override
-//        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-//            mode.getMenuInflater().inflate(R.menu.menu_details, menu);
-//            return true;
-//        }
-//        @Override
-//        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-//            return false;
-//        }
-//        @Override
-//        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-//            switch (item.getItemId()) {
-//                case R.id.delete_all:
-//                    FinAdapter.deleteAllSelected();
-//                    mode.finish();
-//                    return true;
-//                case R.id.add_fav:
-//                    Toast.makeText(MainActivity.this, "ToDo", Toast.LENGTH_LONG).show();
-//                case R.id.unselect_all:
-//                    cocktailAdapter.deselectAll();
-//                    mode.finish();
-//                    return true;
-//                case R.id.select_all:
-//                    cocktailAdapter.selectAll();
-//                    return true;
-//                default:
-//                    return false;
-//            }
-//        }
-//        @Override
-//        public void onDestroyActionMode(ActionMode mode) {
-//            mActionMode = null;
-//        }
-//    };
-
-
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
                 case R.id.delete_all:{
                     finAdapter.deleteAllSelected();
-                    return true;
-
-
-//                    Log.w("accesso a  del", "granted");
-//                    for (int index = 0; index < theList.size(); index++) {
-//                        if(selectedList.get(index)) {
-//                            String id = theList.get(index);
-//                            myDB.deletedata(id);
-//                            theList.remove(index);
-//                            selectedList.delete(index);
-//
-//                        }
-//                    }
-//                    //theList.clear();
-//                    //selectedList.clear();
-//                    listAdapter.notifyDataSetChanged();
-//                    Log.w("CA", "DELETE");
-                //break;
+                    break;
                 }
-
-
                 case R.id.unselect_all:{
                     finAdapter.deselectAll();
-
-//                    selectedList.clear();
-//
-//                    notifyDataSetChanged();
-
-//                    for(int i = 0; i < theList.size()-1; i++){
-//                        selectedList.delete(i);
-//
-//                    }
-//                    selectedList.clear();
-//                    listAdapter.notifyDataSetChanged();
-                    return true;
+                    break;
                 }
-
-
                 case R.id.select_all:{
                     finAdapter.selectAll();
-//                    for(int i = 0; i < theList.size(); i++) {
-//                        selectedList.put(i, true);
-//                    }
-//                    notifyDataSetChanged();
-
-//                    for (int i = 0; i < theList.size(); i++) {
-//                        Log.w("CA", selectedList.size() + "aaaaaaaaaaaaaaaaaaaaaaaaaa");
-//                        selectedList.put(i, true);
-//                    }
-//                    //selectedList.clear();
-//                    listAdapter.notifyDataSetChanged();
-                    //listAdapter.notifyDataSetInvalidated();
-                    //listAdapter.setNotifyOnChange(true);
-                    return true;
+                    break;
                 }
 
         }
@@ -261,10 +99,10 @@ public class ViewListContents extends AppCompatActivity {
 
 
     public class FinAdapter extends RecyclerView.Adapter<Holder> implements View.OnClickListener, View.OnLongClickListener{
-        Cursor dt;
+        ArrayList<String> dt;
 
-        public FinAdapter(Cursor data) {
-            dt = data;
+        public FinAdapter(ArrayList<String> theList) {
+            dt = theList;
         }
 
         @NonNull
@@ -282,9 +120,7 @@ public class ViewListContents extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(@NonNull Holder holder, int position) {
-            if(dt.moveToPosition(position)){
-                holder.tvSave.setText(dt.getString(1));
-                Log.w("CA", "valore" + dt.getString(1));
+                holder.tvSave.setText(dt.get(position));
 
                 boolean isSelected = selectedList.get(position,false);
 
@@ -295,66 +131,61 @@ public class ViewListContents extends AppCompatActivity {
                     holder.tvSave.setSelected(false);
                     holder.tvSave.setTypeface(null, Typeface.NORMAL);
                 }
-            }
         }
 
         @Override
         public int getItemCount() {
-            return dt.getCount();
+            return dt.size();
         }
 
         @Override
         public void onClick(View v) {
+            int position = ((RecyclerView) v.getParent()).getChildAdapterPosition(v);
             Intent intent = new Intent(ViewListContents.this, showDetails.class);
-            intent.putExtra("details", dt.getString(1));
+            intent.putExtra("details", dt.get(position));
             ViewListContents.this.startActivity(intent);
         }
 
         @Override
         public boolean onLongClick(View v) {
             int position = ((RecyclerView) v.getParent()).getChildAdapterPosition(v);
-            boolean isSelected = selectedList.get(position, false);
-            if(isSelected) {
+            boolean isSel = selectedList.get(position, false);
+            if(isSel) {
                 v.setSelected(false);
                 selectedList.delete(position);
             } else {
                 v.setSelected(true);
                 selectedList.put(position, true);
             }
-//            if (mListener != null) {
-//                mListener.onSelect(selectedList.size());  // Callback verso MainActivity
-//            }
             notifyDataSetChanged();
             return true;
         }
 
         public void deselectAll(){
             selectedList.clear();
-            Log.w("CA", "UNSELECT ALL");
             notifyDataSetChanged();
         }
 
         public void selectAll() {
-            for(int i = 0; i < selectedList.size(); i++) {
+            for(int i = 0; i < theList.size(); i++) {
                 selectedList.put(i, true);
             }
-            Log.w("CA", "SELECT ALL");
             notifyDataSetChanged();
         }
 
         public void deleteAllSelected() {
             if (selectedList.size()==0) { return; }
-            for (int index = theList.size()-1; index >=0; index--) {//////////////////////
+            for (int index = theList.size()-1; index >=0; index--) {
                 if (selectedList.get(index,false)) {
-                    Log.w("CA", "DELETE" + theList.get(index));
                     remove(index);
                 }
             }
             selectedList.clear();
-            Log.w("CA", String.valueOf(dt.getCount()));
         }
 
         private void remove(int position) {
+            String id = theList.get(position);
+            myDB.deletedata(id);
             theList.remove(position);
             notifyItemRemoved(position);
         }
