121a4a47367d2a10b406b3ad31c822a7fdaa5059
giogiu98 <gio.giunta@gmail.com>
2020-07-08 16:11:30 +0200

    gio colore rosso

--- app/src/main/java/it/giudevo/worldbank/DataBaseHelper.java
+++ app/src/main/java/it/giudevo/worldbank/DataBaseHelper.java
@@ -15,23 +15,23 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, 1);
     }
 
+
     @Override
     public void onCreate(SQLiteDatabase db) {                                                                           //crea il database e le sue  due tabelle
         String CREATE_TABLE = "create table TABLE_ID " + "( id string primary key, name text )";
         String CREATE_TABLE_DEATAILS = "create table TABLE_VALUE" + "( id string primary key, string text, date text, value text )";
         db.execSQL(CREATE_TABLE_DEATAILS);
         db.execSQL(CREATE_TABLE);
-
-
     }
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {                                        //aggiorna i valori del db
         db.execSQL(" DROP TABLE IF EXISTS  TABLE_ID" );
         db.execSQL(" DROP TABLE IF EXISTS TABLE_VALUE");
         onCreate(db);
-
     }
+
 
     public boolean addData(String string) {                                                         //aggiunge il dato (id) alla prima tabella
         ContentValues contentValues = new ContentValues();
@@ -41,6 +41,7 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         db.close();
         return true;
     }
+
 
     public void addDataDetails(List<Final> name, String string) {                                   // dato un id , aggiunge tutti i singoli dati (data e valore) alla seconda tabella
         SQLiteDatabase db = this.getWritableDatabase();
@@ -60,11 +61,15 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         return db.rawQuery("SELECT * FROM TABLE_ID ", null);
     }
 
+
+
     public Cursor getOneData(String id){                                                            //fa una query al database e ritorna i valori (data e valore) delle righe della seconda tabella
         SQLiteDatabase db  = this.getWritableDatabase();                                            //che hanno un determinato id
         String query = "SELECT * FROM TABLE_VALUE where string =" + "'" + id + "'";
         return db.rawQuery( query , null);
     }
+
+
 
     public Cursor getData(String id){                                                               //fa una query al database e ritorna il valore (id) delle righe della prima tabella
         SQLiteDatabase db  = this.getWritableDatabase();                                            //che hanno un determinato id
@@ -72,8 +77,9 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         return db.rawQuery( query , null);
     }
 
+
+
     public void deletedata (String id){                                                            //data una stringa id , la funzione  elimina da entrambe le tabelle del db tutti i dati releativi ad essa
-       // Log.w("string",id);
         SQLiteDatabase db = this.getWritableDatabase();
         String query = " DELETE FROM TABLE_ID WHERE name =" + "'"+ id +"'";
         String query1 = " DELETE FROM TABLE_VALUE WHERE string =" + "'"+ id +"'";
@@ -81,7 +87,5 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         db.execSQL(query1);
         Log.w("delete","deleted");
         db.close();
-
-
     }
 }
